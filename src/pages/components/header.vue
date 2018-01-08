6fcfdfa7d4ec154df3e02db8b3b043044df6a427
shanguoqiang <guoqiang.shan@healthbok.com>
2018-01-08 22:32:49 +0800

    gx

---
+++ src/pages/components/header.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  .logo
+    img(src="")
+
+  .
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped lang="sass">
+
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+div
+    Head
+</template>
+
+<script>
+import Head from './components/header'
+export default {
+  name: 'index',
+  data(){
+      return {
+
+      }
+  },
+  components: {
+    Head
+  }
+}
+</script>
+
